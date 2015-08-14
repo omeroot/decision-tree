0dc7fde850f7c24cbf6c231597325254daa32b19
Markakod Development <markakoddevelopment@Markakods-iMac.local>
2015-08-14 17:40:19 +0300

    best split

--- main.py
+++ main.py
@@ -2,14 +2,36 @@ import entropy_shannon
 import split_dataset
 
 def chooseBestFeatureToSplit(dataset):
-	splitted = split_dataset.splitDataSet(dataset,2,"kocaeli")
-	result = entropy_shannon.calcShannonEnt(splitted)
-	print "shannon calculated: ",result
+	numFeatures = len(dataset[0])
+	baseEntropy = entropy_shannon.calcShannonEnt(dataset);
+	bestInfoGain = 0.0
+	bestFeature = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataset]
+		uniqueVals = set(featList)	
+		newEntorpy = 0.0
+		for value in uniqueVals:
+			subDataSet = split_dataset.splitDataSet(dataset,i,value)
+			prob = len(subDataSet) / float(len(dataset))
+			newEntorpy += prob * entropy_shannon.calcShannonEnt(subDataSet)
+		infoGain = baseEntropy - newEntorpy
+		if(infoGain > bestInfoGain):
+			bestInfoGain = infoGain
+			bestFeature = i
+	return bestFeature
 	
 if __name__ == '__main__':
 	dataset = [["omer", "demircan", "kocaeli"],
 		["eray", "arslan", "sakarya"],
 		["ss", "dd", "izmir"],
+		["yunus", "kocyigit", "kocaeli"],
+		["omer", "demircan", "kocaeli"],
+		["eray", "arslan", "sakarya"],
+		["ss", "dd", "izmir"],
+		["yunus", "kocyigit", "kocaeli"],
+		["omer", "demircan", "kocaeli"],
+		["eray", "arslan", "sakarya"],
+		["ss", "dd", "izmir"],
 		["yunus", "kocyigit", "kocaeli"]]
-	chooseBestFeatureToSplit(dataset)
+	print chooseBestFeatureToSplit(dataset)
 	
